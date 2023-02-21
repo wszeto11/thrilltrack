75cf4cde159622b846f4a388371a0c35264b06a3
Willard Szeto <wszeto11@gmail.com>
2023-02-21 18:02:06 -0500

    Home.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,17 @@
 import './App.css'
-import Nav from './components/Nav'
-import About from './components/About'
-import Reviews from './components/Reviews'
-import AddReview from './components/AddReview'
-import EditReview from './components/EditReview'
-import { Routes, Route } from 'react-router-dom'
-import ThemeParkList from './components/ThemeParkList'
-import CreateReview from './components/createReview'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      <Nav />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/themeparks" element={<ThemeParkList />} />
-          <Route path="/add-review" element={<AddReview />} />
-          <Route path="/themeparks/:id/reviews" element={<Reviews />} />
-          <Route path="/themeparks/:id/reviews/:id" />
-          <Route path="/review/:id/edit" element={<EditReview />} />
-        </Routes>
-      </main>
+      <div>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   )
 }

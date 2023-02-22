b7e2e40094d139d47d67ddd378a6dc4e41fa2b21
Clifton Lucas <cliftonlucas89@gmail.com>
2023-02-21 20:03:11 -0600

    file update changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Details from './pages/Details'
-import AddReview from './pages/AddReview'
+import { Route, Routes } from 'react-router-dom'
+import React from 'react'
 import Header from './components/Header'
-import CreateReview from './components/CreateReview'
+import CreateReview from './components/createReview'
 import About from './pages/About'
-import ReviewsList from './pages/ReviewsList'
+
+
+
+import About from './pages/About'
+// import Reviews from './components/Reviews'
+// import AddReview from './components/AddReview'
+// import EditReview from './components/EditReview'
+import { Routes, Route } from 'react-router-dom'
+// import ThemeParkList from './components/ThemeParkList'
+import CreateReview from './components/createReview'
+
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
+
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/themepark/:id" element={<Details />} />
-          <Route path="/about/" element={<About />} />
-          <Route path="/themeparks/:id/reviews" element={<ReviewsList />} />
-          <Route path="/themeparks/:id/reviews/addreview" element={<AddReview />} />
+          <Route path="/createReview/" element={<CreateReview />} />
+          <Route path="/About/" element={<About />} />
+      {/* <Nav /> */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/themeparks" element={<ThemeParkList />} /> */}
+          {/* <Route path="/add-review" element={<AddReview />} /> */}
+          {/* <Route path="/themeparks/:id/reviews" element={<Reviews />} /> */}
+          {/* <Route path="/themeparks/:id/reviews/:id" /> */}
+          {/* <Route path="/review/:id/edit" element={<EditReview />} /> */}
+
         </Routes>
       </main>
 

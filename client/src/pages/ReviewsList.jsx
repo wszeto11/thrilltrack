356557c0fc2d06cfbcdf4d3ef64c9cb61ed0770a
Tina Yang <Tinayangers@gmail.com>
2023-02-21 21:35:55 -0500

    Updated changes to home and reviews list

--- client/src/pages/ReviewsList.jsx
+++ client/src/pages/ReviewsList.jsx
@@ -1,6 +1,12 @@
 const ReviewsList = () => {
+    // get all reveiws using axios
+    //use state, use effect
     return (
-        <div><h1>Reviews here</h1></div>
+        <div><h1>Reviews here
+        </h1>
+            {/*  map out reviews */}
+        </div>
+
     )
 }
 export default ReviewsList

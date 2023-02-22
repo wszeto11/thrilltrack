7b3f1f119f69f6602cae572196c3c26fe238dcc6
Tina Yang <Tinayangers@gmail.com>
2023-02-21 20:19:07 -0500

    Added about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
 import './App.css'
-import Nav from './components/Nav'
-import About from './components/About'
-import Reviews from './components/Reviews'
-import AddReview from './components/AddReview'
-import EditReview from './components/EditReview'
+// import Nav from './components/Nav'
+import About from './pages/About'
+// import Reviews from './components/Reviews'
+// import AddReview from './components/AddReview'
+// import EditReview from './components/EditReview'
 import { Routes, Route } from 'react-router-dom'
-import ThemeParkList from './components/ThemeParkList'
+// import ThemeParkList from './components/ThemeParkList'
 import CreateReview from './components/createReview'
 
 
 const App = () => {
   return (
     <div>
-      <Nav />
+      {/* <Nav /> */}
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/about" element={<About />} />
-          <Route path="/themeparks" element={<ThemeParkList />} />
-          <Route path="/add-review" element={<AddReview />} />
-          <Route path="/themeparks/:id/reviews" element={<Reviews />} />
-          <Route path="/themeparks/:id/reviews/:id" />
-          <Route path="/review/:id/edit" element={<EditReview />} />
+          {/* <Route path="/themeparks" element={<ThemeParkList />} /> */}
+          {/* <Route path="/add-review" element={<AddReview />} /> */}
+          {/* <Route path="/themeparks/:id/reviews" element={<Reviews />} /> */}
+          {/* <Route path="/themeparks/:id/reviews/:id" /> */}
+          {/* <Route path="/review/:id/edit" element={<EditReview />} /> */}
         </Routes>
       </main>
     </div>

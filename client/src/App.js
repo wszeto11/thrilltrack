0a00ad46209c3624c5b60826be969af94001dcc6
Clifton Lucas <cliftonlucas89@gmail.com>
2023-02-21 18:45:51 -0600

    createreview update, nnav bar update

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
 import './App.css'
-import Nav from './components/Nav'
-import About from './components/About'
-import Reviews from './components/Reviews'
-import AddReview from './components/AddReview'
-import EditReview from './components/EditReview'
-import { Routes, Route } from 'react-router-dom'
-import ThemeParkList from './components/ThemeParkList'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import React from 'react'
+import Header from './components/Header'
 import CreateReview from './components/createReview'
-
+import About from './pages/About'
 
 const App = () => {
   return (
     <div>
-      <Nav />
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/themeparks" element={<ThemeParkList />} />
-          <Route path="/add-review" element={<AddReview />} />
-          <Route path="/themeparks/:id/reviews" element={<Reviews />} />
-          <Route path="/themeparks/:id/reviews/:id" />
-          <Route path="/review/:id/edit" element={<EditReview />} />
+          <Route path="/createReview/" element={<CreateReview />} />
+          <Route path="/About/" element={<About />} />
         </Routes>
       </main>
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,19 +3,23 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    
+      <header>
+          <nav>
         <div>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/createReview">Add a Review</NavLink>
-          <NavLink to="/reviewListings">Review Listings</NavLink>
+          <NavLink to="/themePark">List of theme Parks</NavLink>
+
+       
         </div>
       
-        
-      </nav>
-    </header>
+        </nav>
+      </header>
+    
   )
 }
 
 export default Header
+

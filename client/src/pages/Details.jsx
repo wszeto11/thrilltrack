e189a0959a82f525522861b40100b15e94b48956
Willard Szeto <wszeto11@gmail.com>
2023-02-22 11:19:19 -0500

    Details.jsx cleaned up

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -1,35 +1,41 @@
-import { useLocation, useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { useState, useEffect } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
+// import { useLocation, useNavigate } from 'react-router-dom'
+// import axios from 'axios'
+// import { useState, useEffect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 const BASE_URL = `http://localhost:3001`
 
-const Details = () => {
+const Details = ({themePark}) => {
 
-    const [details, setDetails] = useState([])
-    let { id } = useParams()
+    // const [details, setDetails] = useState([])
+    // let { id } = useParams()
 
-    const getDetails = async () => {
-        const response = await axios.get(`${BASE_URL}/themepark/:id`)
-        setDetails(response.data.details)
-    }
+    // const getDetails = async () => {
+    //     const response = await axios.get(`${BASE_URL}/themepark/:id`)
+    //     setDetails(response.data.details)
+    // }
 
-    useEffect(() => {
-        getDetails()
-    }, [])
+    // useEffect(() => {
+    //     getDetails()
+    // }, [])
 
     return (
         <div>
             <h1 className='details'>Details</h1>
-            {details.map((detail) => (
-                <div key={detail._id} className='details'>
-                    <p>{detail.name}</p>
-                    <p>Location: {detail.location}</p>
-                    <p>Description: {detail.description}</p>
-                    <p>Rating: {detail.rating}</p>
-                </div>
-            ))}
+            {/* {details.map((detail) => (
+                <div key={detail._id} className='details'> */}
+                    <p>{themePark.name}</p>
+                    <p>Location: {themePark.location}</p>
+                    <p>Description: {themePark.description}</p>
+                    <p>Rating: {themePark.rating}</p>
+                    <Link to = {`/themeparks/${themePark._id}/reviews`} state={{themePark}}>
+                    <h1>
+                        Click here for reviews!
+                    </h1>
+                    </Link>
+                {/* </div>
+            ))} */}
         </div>
     )
 }

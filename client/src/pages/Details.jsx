6e85de2ccf64b10ed9092121edd025a181ffd2a1
Willard Szeto <wszeto11@gmail.com>
2023-02-22 11:07:39 -0500

    Details.jsx updated

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -1,14 +1,35 @@
 import { useLocation, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
+const BASE_URL = `http://localhost:3001`
 
 const Details = () => {
-    let navigate = useNavigate()
-    const location = useLocation()
-    const { themepark } = location.state
-    console.log(themepark)
+
+    const [details, setDetails] = useState([])
+    let { id } = useParams()
+
+    const getDetails = async () => {
+        const response = await axios.get(`${BASE_URL}/themepark/:id`)
+        setDetails(response.data.details)
+    }
+
+    useEffect(() => {
+        getDetails()
+    }, [])
 
     return (
         <div>
-            <h1>Details</h1>
+            <h1 className='details'>Details</h1>
+            {details.map((detail) => (
+                <div key={detail._id} className='details'>
+                    <p>{detail.name}</p>
+                    <p>Location: {detail.location}</p>
+                    <p>Description: {detail.description}</p>
+                    <p>Rating: {detail.rating}</p>
+                </div>
+            ))}
         </div>
     )
 }

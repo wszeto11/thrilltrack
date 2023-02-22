b5fe56e1610d0aa267c8ce9457833f37ae7663e1
Tina Yang <Tinayangers@gmail.com>
2023-02-22 17:32:15 -0500

    Added container for details css

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -7,19 +7,21 @@ const Details = () => {
     // console.log(park)
 
     return (
-        <div>
-            <h1 className='details'>Details</h1>
-            <h1>{park.name}</h1>
-            <img src={park.image} alt='theme park image' />
-            <p>Location: {park.location}</p>
-            <p>Description: {park.description}</p>
-            <p>Rating: {park.rating}</p>
-            <Link to={`/themepark/${park._id}/reviews`} state={{ park }}>
-                <h1>
-                    Click here for reviews!
-                </h1>
-            </Link>
-
+        <div className='details-container'>
+            <div className='details-contain'>
+                <div className='contain'>
+                    <h1 className='details'>{park.name}</h1>
+                    <img src={park.image} alt='theme park image' />
+                    <p>Location: {park.location}</p>
+                    <p>Description: {park.description}</p>
+                    <p>Rating: {park.rating}</p>
+                    <Link to={`/themepark/${park._id}/reviews`} state={{ park }}>
+                        <h1>
+                            Click here for reviews!
+                        </h1>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }

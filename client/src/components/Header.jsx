1f0c0ef737783c149961732dd27a28337a877048
Tina Yang <Tinayangers@gmail.com>
2023-02-23 11:18:09 -0500

    Updated about page css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <NavLink to="/" className='logo'>
               <img src='https://i.imgur.com/cLo3FJS.png' alt='theme park image' className='logoImage' />
             </NavLink>
-            <h1 className='trillTrack'>TrillTrack</h1>
+            <h1 className='trillTrack'>ThrillTrack</h1>
           </div>
           <div className='homeAbout'>
             <div className='homeNav'>
